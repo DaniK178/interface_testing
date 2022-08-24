caceee1e8681cdd5cddcedd47c19d73b8fdc066f
Hanqing Huang <h.hanqing@yahoo.com>
2022-08-24 15:15:47 +0100

    add methods to tiger

--- src/main/java/model/Tiger.java
+++ src/main/java/model/Tiger.java
@@ -1,10 +1,12 @@
 package model;
 
 import model.interfaces.IAnimal;
+import model.interfaces.IHunt;
+import model.interfaces.IRun;
 import model.interfaces.ISwim;
 
 // Hint: missing an interface and a method implementation
-public class Tiger implements ISwim, IAnimal {
+public class Tiger implements ISwim, IAnimal, IRun, IHunt {
     @Override
     public void sleep() {
         System.out.println("Tiger is sleeping");
@@ -13,4 +15,15 @@ public class Tiger implements ISwim, IAnimal {
     public void swim() {
         System.out.println("Tiger is swimming");
     }
+
+    @Override
+    public void run() {
+            System.out.println("Tiger is running");}
+
+    @Override
+    public void eat() {
+        System.out.println("Tiger is eating");
+    }
 }
+
+
